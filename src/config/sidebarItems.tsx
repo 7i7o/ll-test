505f629623be87779b182b142d3aeebfab1939c2
7i7o <mparij@communitylabs.com>
2024-11-01 23:22:48 +0100

    delete files

--- src/config/sidebarItems.tsx
+++ src/config/sidebarItems.tsx
@@ -1,12 +1,3 @@
-import { UploadFile } from '../components/UploadFile';
-import { SendAR } from '../components/SendAR';
-import { EncryptDecrypt } from '../components/EncryptDecrypt';
-import { SendAOToken } from '../components/SendAOToken';
-import { SendAOMessage } from '../components/SendAOMessage';
-import { DryRunAOMessage } from '../components/DryRunAOMessage';
-import { BatchTest } from '../components/BatchTest';
-import { BoltTest } from '../components/BoltTest';
-import { ArIOTest } from '../components/ArIOTest';
 import { LlamaTest } from '../components/LlamaTest';
 import { LlamaTestSimple } from '../components/LlamaTestSimple';
 
